23d8b19de018c1109d3a9c82377bbea0fed55ac5
Chris Pulman <chris.pulman@yahoo.com>
2023-06-25 18:23:19 +0100

    Use CP.Nuke.BuildTools update as required

--- build/Build.cs
+++ build/Build.cs
@@ -1,32 +1,15 @@
-using System;
-using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
 using Nuke.Common.CI.GitHubActions;
-using Nuke.Common.Execution;
 using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.NerdbankGitVersioning;
-using Nuke.Common.Utilities.Collections;
 using Nuke.Common.Tools.DotNet;
 using Serilog;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
-using static Nuke.Common.Tooling.ProcessTasks;
-using System.Collections.Generic;
 using Nuke.Common.Tools.PowerShell;
-using System.Net;
-using System.Text.Json;
-using System.IO;
-using System.Net.Http;
-using System.Net.Http.Json;
-using System.Text;
-using System.Text.Json.Nodes;
-using System.Threading.Tasks;
+using CP.BuildTools;
 
 [GitHubActions(
     "BuildOnly",
@@ -51,26 +34,24 @@ partial class Build : NukeBuild
 
     public static int Main() => Execute<Build>(x => x.Compile);
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     [GitRepository] readonly GitRepository Repository;
     [Solution(GenerateProjects = true)] readonly Solution Solution;
     [NerdbankGitVersioning] readonly NerdbankGitVersioning NerdbankVersioning;
     [Parameter][Secret] readonly string NuGetApiKey;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
     AbsolutePath PackagesDirectory => RootDirectory / "output";
-    string PublicNuGetSource => "https://api.nuget.org/v3/index.json";
 
     Target Print => _ => _
-        .Executes(() =>
-        {
-            Log.Information("NerdbankVersioning = {Value}", NerdbankVersioning.NuGetPackageVersion);
-        });
+        .Executes(() => Log.Information("NerdbankVersioning = {Value}", NerdbankVersioning.NuGetPackageVersion));
 
     Target Clean => _ => _
         .Before(Restore)
-        .Executes(() =>
+        .Executes(async () =>
         {
             if (IsLocalBuild)
             {
@@ -78,8 +59,8 @@ partial class Build : NukeBuild
             }
 
             PackagesDirectory.CreateOrCleanDirectory();
-            UpdateVisualStudio();
-            InstallDotNetSdk("6.x.x", "7.x.x");
+            await this.UpdateVisualStudio();
+            await this.InstallDotNetSdk("6.x.x", "7.x.x");
         });
 
     Target Restore => _ => _
@@ -88,18 +69,15 @@ partial class Build : NukeBuild
         {
             DotNetRestore(s => s.SetProjectFile(Solution));
 
-            RestoreSolutionWorkloads(Solution);
+            Solution.RestoreSolutionWorkloads();
         });
 
     Target Compile => _ => _
         .DependsOn(Restore, Print)
-        .Executes(() =>
-        {
-            DotNetBuild(s => s
+        .Executes(() => DotNetBuild(s => s
                 .SetProjectFile(Solution)
                 .SetConfiguration(Configuration)
-                .EnableNoRestore());
-        });
+                .EnableNoRestore()));
 
     Target Pack => _ => _
     .After(Compile)
@@ -108,12 +86,12 @@ partial class Build : NukeBuild
     {
         if (Repository.IsOnMainOrMasterBranch())
         {
-            var packableProjects = GetPackableProjects();
+            var packableProjects = Solution.GetPackableProjects();
 
-            packableProjects.ForEach(project =>
+            packableProjects?.ForEach(project =>
             {
                 Log.Information("Restoring workloads of {Input}", project);
-                RestoreProjectWorkload(project);
+                project.RestoreProjectWorkload();
             });
 
             DotNetPack(settings => settings
@@ -133,113 +111,10 @@ partial class Build : NukeBuild
         if (Repository.IsOnMainOrMasterBranch())
         {
             DotNetNuGetPush(settings => settings
-                        .SetSource(PublicNuGetSource)
+                        .SetSource(this.PublicNuGetSource())
                         .SetApiKey(NuGetApiKey)
-                        .CombineWith(PackagesDirectory.GlobFiles("*.nupkg").NotEmpty(), (s, v) => s.SetTargetPath(v)),
+                        .CombineWith(PackagesDirectory.GlobFiles("*.nupkg"), (s, v) => s.SetTargetPath(v)),
                     degreeOfParallelism: 5, completeOnFailure: true);
         }
     });
-
-    static void UpdateVisualStudio(string version = "Enterprise")
-    {
-        StartShell($@"dotnet tool update -g dotnet-vs").AssertZeroExitCode();
-        StartShell($@"vs where release").AssertZeroExitCode();
-        StartShell($@"vs update release {version}").AssertZeroExitCode();
-        StartShell($@"vs modify release {version} +mobile +desktop +uwp +web").AssertZeroExitCode();
-        StartShell($@"vs where release").AssertZeroExitCode();
-    }
-
-    static async void InstallDotNetSdk(params string[] versions)
-    {
-        var versionsToInstall = new List<int[]>();
-        var lookupVersions = versions.Select(v => (v, v.Split('.').Select(x =>
-        {
-            return x == "x" || !int.TryParse(x, out var i) ? default(int?) : i;
-        }).ToArray())).ToList();
-
-        var json_data = string.Empty;
-        // attempt to download JSON data as a string
-        try
-        {
-            json_data = await GetFileFromUrlAsync("https://raw.githubusercontent.com/dotnet/core/main/release-notes/releases-index.json");
-
-            var releasesArray = JsonNode.Parse(json_data).Root["releases-index"].AsArray();
-
-            // Find the closest version to the one we want to install
-            foreach (var version in lookupVersions)
-            {
-                var closestVersion = releasesArray.Where(x =>
-                {
-                    // check if the version is not a preview version and if the major version matches
-                    var relver = x["latest-sdk"].ToString();
-                    return !relver.Contains("preview") && version.Item2[0].Equals((relver.Split('.').Select(int.Parse).ToArray()[0]));
-                }).OrderBy(x => Math.Abs(x["latest-sdk"].ToString().CompareTo(version.v))).First();
-                var ver = closestVersion["latest-sdk"].ToString();
-                var verSplit = ver.Split('.').Select(int.Parse).ToArray();
-
-                // check if the version is already in the list
-                if (versionsToInstall.Any(x => x[0] == verSplit[0] && x[1] == verSplit[1] && x[2] == verSplit[2]))
-                {
-                    continue;
-                }
-
-                // check if the version is higher than the one we want to install
-                if (verSplit[1] > version.Item2[1])
-                {
-                    if (version.Item2[1].HasValue)
-                    {
-                        verSplit[1] = version.Item2[1].Value;
-                    }
-                }
-
-                if (verSplit[2] > version.Item2[2])
-                {
-                    if (version.Item2[2].HasValue)
-                    {
-                        verSplit[2] = version.Item2[2].Value;
-                    }
-                    else
-                    {
-                        // TODO: if the minor version is not specified, then we want the latest.T
-                        // The output must be a string, must be three digits, and must be padded with xx if not three digits
-                    }
-                }
-
-                versionsToInstall.Add(verSplit);
-            }
-
-            // if versionsToInstall is empty, then we didn't find any versions to install
-            if (versionsToInstall.Count == 0)
-            {
-                Log.Information("No matching versions found to install");
-            }
-        }
-        catch (Exception ex)
-        {
-            Log.Information("Error downloading JSON data: {Value}", ex.Message);
-        }
-
-
-        StartShell($@"powershell -NoProfile -ExecutionPolicy unrestricted -Command Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile 'dotnet-install.ps1';").AssertZeroExitCode();
-        foreach (var version in versionsToInstall.Select(arr => $"{arr[0]}.{arr[1]}.{arr[2].ToString().First().ToString()}xx").ToArray())
-        {
-            Console.WriteLine($"Installing .NET SDK {version}");
-            StartShell($@"powershell -NoProfile -ExecutionPolicy unrestricted -Command ./dotnet-install.ps1 -Channel '{version}';").AssertZeroExitCode();
-        }
-    }
-
-    static async Task<string> GetFileFromUrlAsync(string url)
-    {
-        using (var httpClient = new HttpClient())
-        {
-            var response = await httpClient.GetAsync(url);
-            return await response.Content.ReadAsStringAsync();
-        }
-    }
-
-    void RestoreProjectWorkload(Project project) => StartShell($@"dotnet workload restore --project {project.Path}").AssertZeroExitCode();
-
-    void RestoreSolutionWorkloads(Solution solution) => StartShell($@"dotnet workload restore {solution}").AssertZeroExitCode();
-
-    List<Project> GetPackableProjects() => Solution.AllProjects.Where(x => x.GetProperty<bool>("IsPackable")).ToList();
 }
