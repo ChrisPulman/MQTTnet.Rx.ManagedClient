86abbe8e2b294dbcd4ed227f87112e13c9cffac9
Chris Pulman <chris.pulman@yahoo.com>
2023-07-06 00:02:39 +0100

    Update SDK for core 3

--- build/Build.cs
+++ build/Build.cs
@@ -60,7 +60,7 @@ partial class Build : NukeBuild
 
             PackagesDirectory.CreateOrCleanDirectory();
             await this.UpdateVisualStudio();
-            await this.InstallDotNetSdk("3.x.x", "5.x.x", "6.x.x", "7.x.x");
+            await this.InstallDotNetSdk("3.1.x", "5.x.x", "6.x.x", "7.x.x");
         });
 
     Target Restore => _ => _
